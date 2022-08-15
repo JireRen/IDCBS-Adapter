be5a7b1381debb710df306e0882ced880056241f
JireRen <jingyaor@usc.edu>
2022-08-15 14:01:27 -0700

    example

---
+++ example/idcbs_adapter.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jire on 8/15/22.
+//
+
+#include "idcbs_adapter.h"
---
+++ example/idcbs_adapter.h
@@ -0,0 +1,8 @@
+//
+// Created by jire on 8/15/22.
+//
+
+#ifndef DISJOINT_CBSH_IDCBS_ADAPTER_H
+#define DISJOINT_CBSH_IDCBS_ADAPTER_H
+
+#endif //DISJOINT_CBSH_IDCBS_ADAPTER_H
